eecc8d5700bbb232bd4e4290d6a1b698c8be846d
Yu <44886954+taofennanhai@users.noreply.github.com>
2024-08-15 13:26:00 +0800

    Initial commit

---
+++ gulimall-member/src/test/java/edu/shu/gulimail/member/GulimailMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.shu.gulimail.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
